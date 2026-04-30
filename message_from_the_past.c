6976786b133889968b2559b9d6875fd483d60ade
tyara24 <tyararamadhani87@gmail.com>
2026-04-30 13:56:40 +0700

    Add anonymous message

--- message_from_the_past.c
+++ message_from_the_past.c
@@ -8,4 +8,11 @@ int main() {
     printf("Kalau udah sukses jangan lupain aslab kalian ya :( \n");
     printf("ID Lines aslab 23: \n CH : christianhadiwijaya \n DS: daffsf \n MF: macropp\n JD: jesayadavidp\n BH: bnhrd\n\n");
     printf("ID Lines aslab 24: \n NA : 123dodol123 \n NZ: naputrizu \n DY: hikari.akira_\n");
+    printf("\n");
+    
+printf("From: Anonymous\n");
+printf("Halo aku di masa depan!\n");
+printf("Semoga kamu sudah mencapai apa yang dulu kamu impikan.\n");
+printf("Kalau lagi capek, ingat kamu sudah sejauh ini.\n");
+printf("Jangan menyerah, pelan-pelan juga nggak apa-apa.\n");
 }
